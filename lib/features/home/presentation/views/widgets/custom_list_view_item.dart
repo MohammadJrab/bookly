4c45e70dc4e8da72ae2a1776d2428478ad0b719f
Mohammad Jrab <mo.jrab02@gmail.com>
2023-07-23 21:30:46 +0300

    Create Styles file and add tiltle to Home

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -11,7 +11,7 @@ class FeaturedListViewItem extends StatelessWidget {
       aspectRatio: 2.7 / 4,
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(16),
           color: Colors.red,
           image: const DecorationImage(
             fit: BoxFit.fill,
