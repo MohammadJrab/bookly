6091d2b23c5ae98217ab5986c4b76e374ddea675
Mohammad Jrab <mo.jrab02@gmail.com>
2023-07-25 15:28:50 +0300

    Create details_book_view.dart

---
+++ lib/features/home/presentation/views/details_book_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetialsBookView extends StatelessWidget {
+  const DetialsBookView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
