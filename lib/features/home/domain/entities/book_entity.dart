e41a72a18b612ce52038e83b584ea3bf90e0b5b2
Mohammad Jrab <mo.jrab02@gmail.com>
2023-07-29 19:46:27 +0300

    create book_entity & model

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,15 @@
 class BookEntity {
-  final String image;
+  final String bookId;
+  final String? image;
   final String title;
-  final String autherName;
-  final num price;
-  final num rating;
-
-  BookEntity(this.image, this.title, this.autherName, this.price, this.rating);
+  final String? autherName;
+  final num? price;
+  final num? rating;
+  BookEntity(
+      {required this.image,
+      required this.title,
+      required this.autherName,
+      required this.price,
+      required this.rating,
+      required this.bookId});
 }
